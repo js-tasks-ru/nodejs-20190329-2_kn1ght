7dc0cfc5b671a90779ce0df755161a726cc29312
tobolovea <tobolovea@skbkontur.ru>
2019-05-11 23:16:20 +0500

    deleteRequest

---
+++ 4-module/3-task/deleteRequest.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const deleteRequest = (pathToFile, res) => {
+  fs.unlink(pathToFile, e => {
+    if (e) {
+      console.error(e);
+      res.statusCode = 404;
+      res.end('File not found');
+    } else {
+      res.statusCode = 200;
+      res.end('ok');
+    }
+  });
+};
+
+module.exports = deleteRequest;
