e3acfb60b5d7e48ffb4cd2971063ea79f4fa3c21
tobolovea <tobolovea@skbkontur.ru>
2019-05-14 04:01:57 +0500

    koa чат

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
+const Chat = require('./chat');
 const app = new Koa();
+const chat = new Chat();
 
 app.use(require('koa-static')('public'));
 app.use(require('koa-bodyparser')());
@@ -7,10 +9,19 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+router.get('/subscribe', async (ctx) => {
+  await new Promise((resolve) => chat.subscribe({resolve, ctx}));
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx) => {
+  const message = ctx.request.body.message;
+  if (message) {
+    chat.send(message);
+    ctx.body = 'ok';
+  } else {
+    ctx.status = 400;
+    ctx.body = 'bad request';
+  }
 });
 
 app.use(router.routes());
